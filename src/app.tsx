fbbc00f768c8682ce5bab436b38964cb4dfc0788
zequemartinez <ezequiel.martinez@codigo.ai>
2024-04-22 16:35:36 -0300

    Update h1 text in app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'preact/hooks';
 import preactLogo from './assets/preact.svg';
 import viteLogo from '/vite.svg';
 import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -16,18 +13,7 @@ export function App() {
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
       </div>
-      <h1>Change this</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <h1>New Title</h1>
     </>
   );
 }
